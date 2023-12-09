5a93bffb161668ace48f7c6f30770105700ba1dc
emfermenty <120209911+emfermenty@users.noreply.github.com>
2023-12-09 20:35:03 +0500

    Update string48.cpp

--- lab9/string48.cpp
+++ lab9/string48.cpp
@@ -4,23 +4,26 @@
 
 using namespace std;
 
+string f(string s) {
+    for (int i = 0; s[++i];)
+        if (s[i] == s[0]) s[i] = '.';
+    return s;
+}
+
 int main() {
-	setlocale(LC_ALL, "Russian");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
-	string a;
-	cout << "vvedite stroky ";
-	getline(cin, a);
-
-	char del = a[0];
-	cout << a[0];
-	for (int i = 0; i < a.length(); i++){
-		if (a[i] == del) {
-			a[i] = '.';	
-		}
-	}
-	for (int i = 1; i < a.length(); i++) {
-		cout << a[i];
-	}
+    string s, str;
+    cout << "PRESS TEXT:\n";
+    getline(cin, str);
+    cout << "INFO:\n";
+    str += " ";
+    for (int i = 0; i < str.size(); i++) {
+        s += str[i];
+        if (str[i] == ' ') {
+            s = f(s);
+            cout << s << " ";
+            s = "";
+        }
+    }
+    system("pause");
+    return 0;
 }
