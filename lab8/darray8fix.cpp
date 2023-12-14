cb0562cc4289fa4bff032012b76bb033e9d953bb
emfermenty <120209911+emfermenty@users.noreply.github.com>
2023-12-14 18:58:00 +0500

    Update darray8fix.cpp

--- lab8/darray8fix.cpp
+++ lab8/darray8fix.cpp
@@ -18,7 +18,9 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			if (i < j) {
+				int temp = arr[i][j];
 				arr[i][j] = arr[j][i];
+				arr[j][i] = temp;
 			}
 		}
 	}
