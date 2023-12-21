95c02c3b308d0c50bb5896d4a56a4479df53a54a
emfermenty <120209911+emfermenty@users.noreply.github.com>
2023-12-21 14:52:50 +0500

    Update 2.cpp

--- laba 10/2.cpp
+++ laba 10/2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
     string word;
-    cout << "Введите слово: ";
+    cout << "press word: ";
     cin >> word;
 
     string shuffled_word = word;
@@ -18,19 +18,22 @@ int main() {
     int count = 0;
     for (int i = 0; i < shuffled_word.length(); i++) {
         int j = uniform_int_distribution<>(0, word.length() - 1)(g);
-        swap(shuffled_word[i], shuffled_word[j]);
+        char temp = shuffled_word[i];
+        shuffled_word[i] = shuffled_word[j];
+        shuffled_word[j] = temp;
     }
 
     while (shuffled_word != word) {
         for (int i = 0; i < shuffled_word.length(); i++) {
             int j = uniform_int_distribution<>(0, word.length() - 1)(g);
-            swap(shuffled_word[i], shuffled_word[j]);
+            char temp = shuffled_word[i];
+            shuffled_word[i] = shuffled_word[j];
+            shuffled_word[j] = temp;
         }
         cout << shuffled_word << endl;
         count++;
     }
-   
-    cout << word << std::endl;
+
     cout << "Общее количество выводов: " << count << endl;
 
     return 0;
