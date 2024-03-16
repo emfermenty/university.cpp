8d75f406d3a764e29a3ed80dd33a3ecf309bd789
emfermenty <120209911+emfermenty@users.noreply.github.com>
2024-03-16 12:50:50 +0500

    Add files via upload

---
+++ sem 2/laba 15/ListWork8.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};typedef Node *Pnode;
+
+Node* findNinthElement(Node* head, int n) {
+    Node* current = head;
+    int count = 1;
+
+    while (current != nullptr && count < 9) {
+        current = current->next;
+        count++;
+    }
+
+    return current;
+}
+
+int main() {
+    Node* head = nullptr;
+    Node* current = nullptr;
+    int n;
+    if (n < 9){
+        cout << "Неверное значение ";
+        return 0;
+    }
+    cout << "Введите количество элементов списка ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << "Введите " << i << " элемент ";
+        int r;
+        cin >> r;
+        Node* newNode = new Node{r, nullptr};
+
+        if (head == nullptr) {
+            head = newNode;
+            current = head;
+        } else {
+            current->next = newNode;
+            current = newNode;
+        }
+    }
+
+
+    Node* ninthElement = findNinthElement(head, n);
+
+    cout << "Указатель на девятый элемент: " << ninthElement << endl;
+    current = head;
+    while (head -> next != nullptr){
+        cout << current -> data << " ";
+        current = current ->next;
+        delete head;
+        head = current;
+    }
+    cout << head ->data << " ";
+
+    return 0;
+}
