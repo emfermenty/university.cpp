1a63e5655d6cfc7c36336ff61c180fd449fbda48
emfermenty <120209911+emfermenty@users.noreply.github.com>
2024-03-18 21:22:18 +0500

    Add files via upload

---
+++ sem 2/laba 15/Dynamic10.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+void show(Node* top) {
+    Node* current = top;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;  
+}
+
+void splitStack(Node* P1, Node*& evenStack, Node*& oddStack) {
+    Node* current = P1;
+    evenStack = nullptr;
+    oddStack = nullptr;
+
+    while (current != nullptr) {
+        Node* temp = current;
+        current = current->next;
+
+        if (temp->data % 2 == 0) {
+            temp->next = evenStack;
+            evenStack = temp;
+        }
+        else {
+            temp->next = oddStack;
+            oddStack = temp;
+        }
+    }
+
+    cout << "Адрес вершины стека с четными значениями: " << evenStack << std::endl;
+    cout << "Адрес вершины стека с нечетными значениями: " << oddStack << std::endl;
+}
+
+int main() {
+    int n;
+    cout << "press amounths ellements of stack ";
+    cin >> n;
+    Node* head = nullptr;
+    Node* current = nullptr;
+    for (int i = 1; i <= n; i++) {
+        int r;
+        cout << "press " << i <<" element of stack ";
+        cin >> r;
+        Node* P1 = new Node{r, nullptr};
+        if (head == nullptr) {
+            head = P1;
+            current = head;
+        }
+        else {
+            current->next = P1;
+            current = P1;
+        }
+    }
+
+    Node* evenStack;
+    Node* oddStack;
+
+    splitStack(head, evenStack, oddStack);
+    show(evenStack);
+    show(oddStack);
+
+    return 0;
+}
