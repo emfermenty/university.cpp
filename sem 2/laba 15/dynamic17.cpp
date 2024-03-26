61b24f2ff25cddedbd41c6207b634a11a5e0bdcf
emfermenty <120209911+emfermenty@users.noreply.github.com>
2024-03-26 18:25:30 +0500

    Add files via upload

---
+++ sem 2/laba 15/dynamic17.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+struct Queue {
+    Node* front;
+    Node* rear;
+};
+
+void enqueue(Queue& queue, int data) {
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = nullptr;
+
+    if (queue.front == nullptr) {
+        queue.front = queue.rear = newNode;
+    }
+    else {
+        queue.rear->next = newNode;
+        queue.rear = newNode;
+    }
+}
+
+void print(Queue& queue) {
+    Node* temp = queue.front;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+    cout << "Адрес начала: " << queue.front << endl;
+    cout << "Адрес конца: " << queue.rear << endl;
+}
+
+void main(){
+    setlocale(LC_ALL, "ru");
+    int d;
+    cout << "Введите кол-во элементов ";
+    cin >> d;
+
+    Queue queue;
+    queue.front = nullptr;
+    queue.rear = nullptr;
+
+    for (int i = 0; i < d; i++) {
+        cout << "Введите " << i << " элемент " << endl;
+        int k;
+        cin >> k;
+        enqueue(queue, k);
+    }
+
+    cout << "Initial " << endl;
+    print(queue);
+
+    int D;
+    cout << "Введите D ";
+    cin >> D;
+    enqueue(queue, D);
+    cout << "after " << endl;
+    print(queue);
+    return;
+}
